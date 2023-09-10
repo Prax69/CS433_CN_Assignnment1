143db6ce8baa788515070e607d9e55afa02e5dd5
SRocks04 <102377764+SRocks04@users.noreply.github.com>
2023-09-10 23:48:29 +0530

    Update 2e.c

--- 2e.c
+++ 2e.c
@@ -1,51 +1,85 @@
 #include <stdio.h>
-#include <pcap.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
-#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <linux/if_ether.h>
 
-#define IP_HEADER_SIZE 20
-#define PCAP_FILE "3.pcap" // enter path to your pcap file or make sure it is in the same directory as your code
+#define PACKET_BUFFER_SIZE 65536
 
-void process_packet(u_char *user_data, const struct pcap_pkthdr *header, const u_char *packet);
+int process_packet(unsigned char *, int);
 
 int main() {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *handle;
+    int raw_socket;
+    struct sockaddr server;
+    socklen_t server_len = sizeof(server);
+    unsigned char packet_buffer[PACKET_BUFFER_SIZE];
 
-    handle = pcap_open_offline(PCAP_FILE, errbuf);
-    if (handle == NULL) {
-        fprintf(stderr, "Error opening pcap file: %s\n", errbuf);
+    // Create a raw socket to capture all packets
+    raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (raw_socket == -1) {
+        perror("Socket creation error");
+        exit(1);
+    }
+
+    // Receive packets and print information
+    while (1) {
+        int packet_size = recvfrom(raw_socket, packet_buffer, PACKET_BUFFER_SIZE, 0, &server, &server_len);
+        if (packet_size == -1) {
+            perror("Packet receive error");
+            close(raw_socket);
+            exit(1);
+        }
+	
+        if (process_packet(packet_buffer, packet_size)) break;
+    }
+
+    close(raw_socket);
+    return 0;
+}
+
+int process_packet(unsigned char *packet, int packet_size) {
+    struct iphdr *ip_header = (struct iphdr *)(packet + sizeof(struct ethhdr));
+    struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct ethhdr) + sizeof(struct iphdr));
+
+    char src_ip[INET_ADDRSTRLEN];
+    char dest_ip[INET_ADDRSTRLEN];
+
+    // Convert source and destination IP addresses to human-readable format
+    inet_ntop(AF_INET, &(ip_header->saddr), src_ip, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(ip_header->daddr), dest_ip, INET_ADDRSTRLEN);
+    
+    packet[packet_size]='\0';
+    char *payload = packet + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr);
+    
+    char *searchstr = "milkshake";
+    char *ptr = strstr(payload, searchstr);
+
+    if (ptr != NULL) {
+    	
+        printf("Found 'Flag' keyword in packet payload:\n");
+        printf("Source IP: %s\n", src_ip);
+        printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
+        printf("Destination IP: %s\n", dest_ip);
+        printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
+        printf("Payload Data:\n");
+
+        // Print payload data, assuming it's ASCII text
+        int start=strstr(payload,"flavor- ") - payload + 8;
+        for (int i = start; i < strlen(payload); i++) {
+            if (payload[i] != ' ') {
+                putchar(payload[i]);
+            } else {
+                break;
+            }
+        }
+        printf("\n");
         return 1;
     }
-
-    const char *search_string = "milkshake";
-
-    struct pcap_pkthdr header;
-    const unsigned char *packet;
-
-    while ((packet = pcap_next(handle, &header)) != NULL) {
-        struct ip *ip_header = (struct ip *)(packet + 14);
-
-        struct tcphdr *tcp_header = (struct tcphdr *)(packet + 14 + IP_HEADER_SIZE);
-
-        int payload_size = header.caplen - (14 + IP_HEADER_SIZE + (tcp_header->th_off << 2));
-        const char *payload_data = (const char *)(packet + 14 + IP_HEADER_SIZE + (tcp_header->th_off << 2));
-
-        if (payload_size > 0 && strstr(payload_data, search_string) != NULL) {
-            printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
-            printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
-            printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
-            printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
-
-            // Print the entire TCP payload data
-            printf("TCP Payload Data:\n");
-            printf("%.*s\n\n", payload_size, payload_data);
-        }
-    }
-
-    pcap_close(handle);
-
     return 0;
 }
